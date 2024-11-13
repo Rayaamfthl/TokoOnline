6cddec2890f5c03214d113d95d326d91cf32c687
Rayaamfthl <insankml19@gmail.com>
2024-11-13 15:31:44 -0800

    first commit

--- resources/views/v_login/login.blade.php
+++ resources/views/v_login/login.blade.php
@@ -1,45 +1,155 @@
 <!DOCTYPE html>
-<html lang="en">
+<html dir="ltr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Toko Online</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="WP2">
+    <meta name="author" content="Miftahul Raya">
+    <meta name="description" content="Project Toko Online Mata Kuliah Web Progamming UBSI">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('backend/image/icon_univ_bsi.png') }}">
+    <title>Toko Online </title>
+    <!-- Custom CSS -->
+    <link href="{{ asset('backend/matrix-admin-master/dist/css/style.min.css') }}" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
 
 <body>
-    <h3>{{ $judul }}</h3>
-    @if (session()->has('error'))
-        <div class="alert alert-danger alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="close"><span
-                    aria-hidden="true">&times;</span></button>
-            <strong>{{ session('error') }}</strong>
+    <div class="main-wrapper">
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
         </div>
-    @endif
-    <form action="{{ route('backend.login') }}" method="post">
-        @csrf
-        <label>User</label><br>
-        <input type="text" name="email" id="" value="{{ old('email') }}"
-            class="form-control @error('email') is-invalid @enderror" placeholder="Masukkan Email">
-        @error('email')
-            <span class="invalid-feedback alert-danger" role="alert">
-                {{ $message }}
-            </span>
-        @enderror
-        <p></p>
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Login box.scss -->
+        <!-- ============================================================== -->
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center bg-dark"
+            style="height: 100vh">
+            <div class="auth-box bg-dark border-top border-secondary" style="width: 60vh">
+                <div id="loginform">
+                    <div class="text-center p-t-20 p-b-20">
+                        <span class="db"><img src="{{ asset('backend/image/logo_text.png') }}" alt="logo">
+                        </span>
+                    </div>
 
-        <label>Password</label><br>
-        <input type="password" name="password" id="" value="{{ old('password') }}"
-            class="form-control @error('password') is-invalid @enderror" placeholder="Masukkan Password">
-        @error('password')
-            <span class="invalid-feedback alert-danger" role="alert">
-                {{ $message }}
-            </span>
-        @enderror
-        <p></p>
+                    @if (session()->has('error'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <strong>{{ session('error') }}</strong>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                aria-label="Close"></button>
+                        </div>
+                    @endif
+                    <!-- Form -->
+                    <form class="form-horizontal m-t-20" id="loginform" action="{{ route('backend.login') }}"
+                        method="POST">
+                        @csrf
+                        <div class="row p-b-30">
+                            <div class="col-12">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-success text-white" id="basic-addon1"><i
+                                                class="ti-user"></i></span>
+                                    </div>
+                                    <input type="text" name="email"
+                                        class="form-control form-control-lg @error('email') is-invalid @enderror"
+                                        placeholder="Masukan Email" aria-label="Email" aria-describedby="basic-addon1">
+                                    @error('email')
+                                        <span class="invalid-feedback alert-danger" role="alert">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-warning text-white" id="basic-addon2"><i
+                                                class="ti-pencil"></i></span>
+                                    </div>
+                                    <input type="password" name="password"
+                                        class="form-control form-control-lg @error('password') is-invalid @enderror"
+                                        placeholder="Masukan Password" aria-label="Password"
+                                        aria-describedby="basic-addon1">
+                                    @error('password')
+                                        <span class="invalid-feedback alert-danger" role="alert">
+                                            {{ $message }}
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row border-top border-secondary">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <div class="p-t-20">
+                                        <button class="btn btn-info" id="to-recover" type="button"><i
+                                                class="fa fa-lock m-r-5"></i> Lost password?</button>
+                                        <button class="btn btn-success float-right" type="submit">Login</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- Login box.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper scss in scafholding.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper scss in scafholding.scss -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right Sidebar -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right Sidebar -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- All Required js -->
+    <!-- ============================================================== -->
+    <script src="{{ asset('backend/matrix-admin-master/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="{{ asset('backend/matrix-admin-master/assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugin js -->
+    <!-- ============================================================== -->
+    <script>
+        $('[data-toggle="tooltip"]').tooltip();
+        $(".preloader").fadeOut();
+        // ============================================================== 
+        // Login and Recover Password 
+        // ============================================================== 
+        $('#to-recover').on("click", function() {
+            $("#loginform").slideUp();
+            $("#recoverform").fadeIn();
+        });
+        $('#to-login').click(function() {
 
-        <button type="submit">Login</button>
+            $("#recoverform").hide();
+            $("#loginform").fadeIn();
+        });
+    </script>
+
 </body>
 
 </html>
